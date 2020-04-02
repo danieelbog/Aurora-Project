69ab169d68b615a3025ef48f03867f973b784953
Danieelbog <danieel.bogdan@gmail.com>
2020-04-02 10:05:05 +0300

    Aurora Project Rebuild Finished

--- AuroraProject/Controllers/AccountControllers/AccountController.cs
+++ AuroraProject/Controllers/AccountControllers/AccountController.cs
@@ -168,7 +168,15 @@ namespace AuroraProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Wallet = new Wallet(1000)
+                };
+                
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
