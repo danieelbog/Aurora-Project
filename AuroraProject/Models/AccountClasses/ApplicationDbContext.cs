2779147e1ad174f055fbf665c216984ced9a2228
Danieelbog <danieel.bogdan@gmail.com>
2020-04-02 08:28:51 +0300

    Basic Influencer and Membership Type Model

--- AuroraProject/Models/AccountClasses/ApplicationDbContext.cs
+++ AuroraProject/Models/AccountClasses/ApplicationDbContext.cs
@@ -14,10 +14,10 @@ namespace AuroraProject.Models
         public DbSet<AdvancedPackage> AdvancedPackages { get; set; }
         public DbSet<PremiumPackage> PremiumPackages { get; set; }
 
-        //public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
         public DbSet<Industry> Industries { get; set; }
         public DbSet<SpecificIndustry> SpecificIndustries { get; set; }
-        //public DbSet<Influencer> Influencers { get; set; }
+        public DbSet<Influencer> Influencers { get; set; }
         //public DbSet<FavouriteGig> FavouriteGigs { get; set; }
         //public DbSet<FavouriteInfluencer> FavouriteInfluencers { get; set; }
         //public DbSet<Wallet> Wallets { get; set; }
---
+++ AuroraProject/Models/Influencer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuroraProject.Models
+{
+    public class Influencer
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string MainLanguage { get; set; }
+
+        [Required]
+        public string MainPlatform { get; set; }
+
+        [Required]
+        public string Exposure { get; set; }
+
+        [Required]
+        public string MainTopic { get; set; }
+
+        [Required]
+        public string AudienceAge { get; set; }
+
+        [Required]
+        public string AudienceMainCountry { get; set; }
+
+        public string AudienceMainState { get; set; }
+
+        [Required]
+        public string AudienceMainTrait { get; set; }
+
+        [Required]
+        public string AboutTheInfluencer { get; set; }
+
+        //RELATION WITH APPLICATION USER
+        //[Required]
+        //public string UserID { get; set; }
+        //public ApplicationUser User { get; set; }
+
+        // RELATION WITH MEMBERSHIPTYPE
+        [Required]
+        public int MembershipTypeID { get; set; }
+        public MembershipType MembershipType { get; set; }
+
+        //RELATION WITH FAVOURITE INFLUENCER
+        //public ICollection<FavouriteInfluencer> Followers { get; set; }
+    }
+}
